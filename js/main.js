2056efd0282d293e21c335465b8750fa8af8185b
Menno <mennodevries1@msn.com>
2019-03-18 13:31:19 +0100

    Code improvements

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
-/********************************
-***                           ***
-******      JavaScript     ******
-***                           ***
-* Copyright (c) 2019 menno.work *
-*********************************/
-const keyContainer = document.querySelector('.key-container')
-const keys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
 const keyCodes = [65, 83, 68, 70, 71, 72, 74, 75, 76]
-const sounds = ['Clap', 'Hihat', 'Kick', 'Openhat', 'Boom', 'Ride', 'Snare', 'Tom', 'Tink']
-const soundFiles = ['clap.wav', 'hihat.wav', 'kick.wav', 'openhat.wav', 'boom.wav', 'ride.wav', 'snare.wav', 'tom.wav', 'tink.wav']
 
-function appendData() {
-  for (let i = 0; i < keys.length; i++) {
-    // Create a new div for every element in "keys" array
-    let key = document.createElement('div')
-    key.className = 'key'
-    // Add the values from keys, sounds and soundFiles 
-    // Note: positions are equal
-    key.insertAdjacentHTML('beforeend', '<span class="letter">' + keys[i] + '</span><span class="sound-name">' + sounds[i] + '</span><audio class="' + sounds[i].toLowerCase() + '"><source src="assets/sounds/' + soundFiles[i] + '" type="audio/wav"></audio>')
-    // Append the newly created divs to the container
-    keyContainer.appendChild(key)
+const appendData = () => {
+  const keyContainer = document.querySelector('.key-container')
+  const keys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
+  const sounds = ['Clap', 'Hihat', 'Kick', 'Openhat', 'Boom', 'Ride', 'Snare', 'Tom', 'Tink']
+  const soundFiles = ['clap.wav', 'hihat.wav', 'kick.wav', 'openhat.wav', 'boom.wav', 'ride.wav', 'snare.wav', 'tom.wav', 'tink.wav']
+
+  for (let i in keys) {
+    const keyElement = document.createElement('div')
+
+    keyElement.className = 'key'
+    keyElement.insertAdjacentHTML('beforeend', 
+      `<span class="letter">${keys[i]}</span>
+       <span class="sound-name">${sounds[i]}</span>
+       <audio class="${sounds[i].toLowerCase()}">
+         <source src="assets/sounds/${soundFiles[i]}" type="audio/wav">
+       </audio>`)
+    keyContainer.appendChild(keyElement)
   }
 }
 
-function playSound(e) {
+const playSound = (e) => {
   for (let i = 0; i < keyCodes.length; i++) {
     // Check if the pushed key(code) is 
     // equal to a keycode in the array
@@ -38,7 +35,7 @@ function playSound(e) {
   }
 }
 
-function removeClass() {
+const removeClass = () => {
   this.classList.remove('active-key')
 }
 
